2e8ee9ac5fa4d56990bed0b645412e3ef9dfd645
Dmitry Chigrin <98915479+ChigrinDmitry@users.noreply.github.com>
2022-04-29 12:06:55 +0500

    Update matrix-elements-sum.js

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -19,7 +19,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getMatrixElementsSum(matrix) {
   let sum = 0;
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; i < matrix[i].length; j++) {
+    for (let j = 0; j < matrix[i].length; j++) {
     sum += [i][j];
     }
   }
