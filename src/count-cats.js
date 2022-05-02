4aba45c0cbef7b98e261aba0afedebaa31c959fd
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2022-05-02 08:56:41 +0500

    feat: "count cats" solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -16,14 +16,15 @@
 function countCats(arr) {
   let counter = 0;
 
-  for (let elem of arr) {
-	  if (elem == '^^') {
-		counter++;
-	}
+  for (let i = 0; i < arr.length; i++) {
+    for ( let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] == "^^") 
+        {counter++}
+    }
+  }
   return (counter);
 }
 
-}
 
 module.exports = {
   countCats
